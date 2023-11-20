addbd822f4056b8041477fd25c1cc6cd9923ea31
brucechow <lzzzzl005@gmail.com>
2023-11-20 23:38:58 +0800

    update

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
+import { useTranslation } from "@/app/i18n";
 
 type PageParams = {
   lng: string;
 };
 
-const Page = ({ lng }: PageParams) => {
+const Page = async ({ params }: { params: { lng: string } }) => {
+  const { t } = await useTranslation(params.lng, "translation");
+  console.log(t);
   return (
     <>
-      <h1>Hi there!</h1>
-      <Link href={`/${lng}/second-page`}>second page</Link>
+      <h1>{t('title')}</h1>
+      <Link href={`/${params.lng}/second-page`}>
+        {t('to-second-page')}
+      </Link>
     </>
   );
 };
--- app/[lng]/second-page/page.tsx
+++ app/[lng]/second-page/page.tsx
@@ -1,20 +1,18 @@
+import { useTranslation } from "@/app/i18n";
 import Link from "next/link";
 
 type SecondPageParams = {
   lng: string;
-}
+};
 
-const SecondPage = ({lng}: SecondPageParams) => {
+const SecondPage = async ({ params }: { params: { lng: string } }) => {
+  const { t } = await useTranslation(params.lng, 'second-page')
   return (
     <>
-      <h1>
-        Hi from second page!
-      </h1>
-      <Link href={`/${lng}`}>
-        back
-      </Link>
+      <h1>{t("title")}</h1>
+      <Link href={`/${params.lng}`}>{t("back-to-home")}</Link>
     </>
   );
-}
- 
+};
+
 export default SecondPage;
